d21fbccf19ad5ccf90c23c34397812d6acf4c016
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-11 16:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	 return key in sampleObject;
+	  return sampleObject.hasOwnProperty(key);
 	
 	}
 }
